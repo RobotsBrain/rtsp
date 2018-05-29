6cb803c4616564774ca35e518a6c7aa6bb23bc96
吴敏 <wumin@tuya.com>
2018-05-29 12:06:52 +0800

    add code

---
+++ src/rtsp_server.h
@@ -0,0 +1,15 @@
+/*
+Copyright (c) 2012, Paula Roquero Fuentes <paula.roquero.fuentes@gmail.com>
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+#ifndef _RTSP_SERVER_H_
+#define _RTSP_SERVER_H_
+
+
+int rtsp_server(unsigned short port, unsigned short rtp_port);
+
+
+#endif
---
+++ test/test_rtsp_server.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdio.h>
+
+#include "rtsp_server.h"
+
+
+
+int main(int argc, char **argv)
+{
+    unsigned short rtsp_port = 2000;
+    unsigned short rtp_port = 2001;
+    int ret;
+
+    if (argc >= 2) {
+        ret = atoi(argv[1]);
+        if (ret > 1024 && ret < 60000) {
+            rtsp_port = ret;
+        }
+    }
+
+    if (argc == 3) {
+        ret = atoi(argv[2]);
+        if (ret > 1024 && ret < 60000)
+            rtp_port = ret;
+    }
+
+    if (rtsp_port == rtp_port) {
+        fprintf(stderr, "Ports must be different\n");
+        return 0;
+    }
+
+    rtsp_server(rtsp_port, rtp_port);
+
+    return(0);
+}
+
+// ffplay rtsp://127.0.0.1:2000 -v debug
+
